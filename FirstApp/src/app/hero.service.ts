046731e1e10329c71d253458477c2dd36fa206f7
Basnet <prashantsingh.basnet@cotiviti.com>
2022-04-24 17:58:15 +0545

    MessageService for loosely-coupled communication between classes

--- FirstApp/src/app/hero.service.ts
+++ FirstApp/src/app/hero.service.ts
@@ -2,20 +2,25 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  //constructor() { }
 
   // getHeroes(): Hero[] {
   //   return HEROES;
   // }
 
+  //injecting MessageService into HeroService which is injected into HeroComponent
+  constructor(private messageService: MessageService) { }
+
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 
--- FirstApp/src/app/heroes/heroes.component.ts
+++ FirstApp/src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 //import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -19,7 +20,7 @@ export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
 
   //constructor() { }
-  constructor(private heroService: HeroService){}
+  constructor(private heroService: HeroService, private messageService:MessageService){}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -27,6 +28,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   //synchronous approach
