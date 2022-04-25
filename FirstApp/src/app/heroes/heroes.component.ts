928205be7c1b6ea5002961aa0b7cb048342fd78f
Basnet <prashantsingh.basnet@cotiviti.com>
2022-04-25 09:04:27 +0545

    Angular router to navigate among diff components. <a> links <router-outlet> used. Configured the router in an AppRoutingModule. Defined routes, a redirect routes, and a parameterized route. Converted tightly-coupled master/detail view into a routed detail view. Shared HeroService among multiple components. Used router link parameters to navigate to the detail view of a user-selected hero.

--- FirstApp/src/app/heroes/heroes.component.ts
+++ FirstApp/src/app/heroes/heroes.component.ts
@@ -17,7 +17,7 @@ export class HeroesComponent implements OnInit {
   //heroes=HEROES;
 
   heroes: Hero[] = [];
-  selectedHero?: Hero;
+ // selectedHero?: Hero;
 
   //constructor() { }
   constructor(private heroService: HeroService, private messageService:MessageService){}
@@ -26,10 +26,10 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  // }
 
   //synchronous approach
  // getHeroes(): void {
