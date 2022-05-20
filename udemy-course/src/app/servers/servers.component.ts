572b2d3106f278dbc97cc612b824c4ad561b5f37
PrashantSBasnet <prasbas97@gmail.com>
2022-05-20 13:38:19 +0545

    Two way data binding -- use of ngModel

--- udemy-course/src/app/servers/servers.component.ts
+++ udemy-course/src/app/servers/servers.component.ts
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
 
   allowNewServer = false;
+  serverCreationStatus= 'No server was created';
+  serverName='TestServer using Two way binding';
+
+
 
   constructor() {
     setTimeout(()=> {
@@ -17,6 +21,15 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus= 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event:any){
+    console.log(event);
+    this.serverName= (<HTMLInputElement>event.target).value;
   }
 }
 
