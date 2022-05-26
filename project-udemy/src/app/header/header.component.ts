62f436788615062fd42a6d13ae6667dd69eec119
PrashantSBasnet <prasbas97@gmail.com>
2022-05-26 15:11:34 +0545

    Passing data with event and property binding

--- project-udemy/src/app/header/header.component.ts
+++ project-udemy/src/app/header/header.component.ts
@@ -6,13 +6,13 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent{
+export class HeaderComponent {
 
   @Output() featureSelected = new EventEmitter<string>();
 
-  onSelect(feature:string){
+  onSelect(feature: string) {
     this.featureSelected.emit(feature);
-    
+
 
   }
 
--- project-udemy/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project-udemy/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,16 +8,23 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+ @Output() recipeWasSelected= new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe("Test Recipe", "this is for testing purpose", "https://www.google.com/search?q=images+recipe&tbm=isch&ved=2ahUKEwi67OHH2vX3AhX2zaACHUaFCQoQ2-cCegQIABAA&oq=images+recipe&gs_lcp=CgNpbWcQAzIFCAAQgAQyBQgAEIAEMgUIABCABDIGCAAQHhAFMgYIABAeEAgyBggAEB4QCDIGCAAQHhAIMgYIABAeEAgyBggAEB4QCDIGCAAQHhAIOgcIABCxAxBDOgQIABBDUPwGWNAMYIoOaABwAHgAgAHsAYgBjAySAQMyLTeYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=YImLYrq8Lfabg8UPxoqmUA&bih=661&biw=1360#imgrc=2ZUZnZVXTOUYuM" )
+ recipes: Recipe[] = [
+     new Recipe("Test Recipe", "this is for testing purpose", "https://www.google.com/search?q=images+recipe&tbm=isch&ved=2ahUKEwi67OHH2vX3AhX2zaACHUaFCQoQ2-cCegQIABAA&oq=images+recipe&gs_lcp=CgNpbWcQAzIFCAAQgAQyBQgAEIAEMgUIABCABDIGCAAQHhAFMgYIABAeEAgyBggAEB4QCDIGCAAQHhAIMgYIABAeEAgyBggAEB4QCDIGCAAQHhAIOgcIABCxAxBDOgQIABBDUPwGWNAMYIoOaABwAHgAgAHsAYgBjAySAQMyLTeYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=YImLYrq8Lfabg8UPxoqmUA&bih=661&biw=1360#imgrc=2ZUZnZVXTOUYuM" )
 ,    new Recipe("Test Recipe", "this is for testing purpose", "https://www.google.com/search?q=images+recipe&tbm=isch&ved=2ahUKEwi67OHH2vX3AhX2zaACHUaFCQoQ2-cCegQIABAA&oq=images+recipe&gs_lcp=CgNpbWcQAzIFCAAQgAQyBQgAEIAEMgUIABCABDIGCAAQHhAFMgYIABAeEAgyBggAEB4QCDIGCAAQHhAIMgYIABAeEAgyBggAEB4QCDIGCAAQHhAIOgcIABCxAxBDOgQIABBDUPwGWNAMYIoOaABwAHgAgAHsAYgBjAySAQMyLTeYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=YImLYrq8Lfabg8UPxoqmUA&bih=661&biw=1360#imgrc=2ZUZnZVXTOUYuM" )
 
   ];
 
+
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+
+  onRecipeSelected(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
