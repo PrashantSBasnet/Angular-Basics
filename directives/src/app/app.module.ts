85c850bc3a999a7e761731f6ef4b169d233991f2
PrashantSBasnet <prasbas97@gmail.com>
2022-05-27 11:53:27 +0545

    Structural custom directives. Added navigation using custom structural diretive in the prj

--- directives/src/app/app.module.ts
+++ directives/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BetterHighlightComponent } from './better-highlight/better-highlight.component';
 import { BasicHighlightsComponent } from './basic-highlights/basic-highlights.component';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BetterHighlightComponent,
-    BasicHighlightsComponent
+    BasicHighlightsComponent,
+    UnlessDirective
   ],
   imports: [
     BrowserModule
--- directives/src/app/better-highlight/better-highlight.component.ts
+++ directives/src/app/better-highlight/better-highlight.component.ts
@@ -1,4 +1,4 @@
-import { OnInit, ElementRef, Renderer2, Directive } from '@angular/core';
+import { OnInit, ElementRef, Renderer2, Directive, HostListener, HostBinding, Input } from '@angular/core';
 @Directive({
   selector: '[appBetterHighlight]]'
 })
@@ -7,7 +7,17 @@ export class BetterHighlightComponent implements OnInit {
   constructor(private elRef: ElementRef, private renderer:Renderer2) { }
 
   ngOnInit(): void {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
   }
+
+  @HostListener('mouseenter') mouseover(eventData: Event)  {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event)  {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+  }
+
 
 }
