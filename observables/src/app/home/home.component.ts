19911127fb7099a4f9ac777b34c60297bc257911
PrashantSBasnet <prasbas97@gmail.com>
2022-06-02 15:38:44 +0545

    Custom Observables

---
+++ observables/src/app/home/home.component.ts
@@ -0,0 +1,45 @@
+
+
+import { Component, OnDestroy, OnInit,  } from '@angular/core';
+import { interval, Subscription, Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit{
+
+  private firstObsSubscription: Subscription | any;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // this.firstObsSubscription= interval(1000).subscribe(count => {
+    //     console.log(count);
+    // })
+    const customIntervalObservable = Observable.create((observer:any) => {
+      let count =0;
+      setInterval(() => {
+        observer.next(count);
+        if (count==4){
+          observer.complete(); //it will come to hault and no values are emitted after this
+        }
+        if (count>5){
+          observer.error(new Error('count is greater than 5')) //it will give error
+        }
+        count++;},
+        1000);
+      });
+
+   this.firstObsSubscription= customIntervalObservable.subscribe((data: any) =>{
+      console.log(data);
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+  }
+
+}
